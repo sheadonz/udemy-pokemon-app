09aded7b90b105e614308852ff353cba598c5010
sheadonz <njxb.441@gmail.com>
2024-04-24 16:41:41 +0900

    3-13

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { getALLPokemon, getPokemon } from './utils/pokemon.js';
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
-
+  const [pokemonData, setPokemonData] = useState()
   useEffect(() => {
     const fetchPokemonData = async () => {
       // すべてのポケモンのデータを取得
@@ -16,16 +16,17 @@ function App() {
     };
     fetchPokemonData();
   }, []);
-  const loadPokemon = (data) => {
-    let _pokemonData = Promise.all(
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
       data.map( (pokemon) => {
         // console.log(pokemon);
-        let pokemonRecord = getALLPokemon(pokemon.url);
+        let pokemonRecord = getPokemon(pokemon.url);
         return pokemonRecord;
       })
     );
+    setPokemonData(_pokemonData);
   }
-
+  console.log(pokemonData);
   return (
     <div className="App">
       {loading ? <h1>Loading...</h1> : <h1>Data loaded</h1>}
